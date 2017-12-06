d9565497afc234c67a0de4e16a9b44715356e993
Miwan9 <kozhema7@gmail.com>
2017-12-06 12:56:06 +0200

    second

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ document.getElementById('btn1').onclick=years;
 function years(){
 	var f=document.getElementById('inp1').value; 
 		f=parseInt(f); 
-	if (f>16) {
+	if (f>=16) {
 			console.log('Добро пожаловать')
 	}
-	if (f<16){
+	else if (f<16){
 		console.log('Вы ещё молоды');
 	}
 	else {
